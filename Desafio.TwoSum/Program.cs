380d9b8b310dec32af89f9b455d544fd788b95f6
hajikazuo <hajikazuo@outlook.com>
2024-09-10 17:23:12 -0300

    Add project files.

---
+++ Desafio.TwoSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+namespace Desafio.TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var test1 = new Solution().TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine($"Test 1: [{test1[0]}, {test1[1]}]");
+
+            var test2 = new Solution().TwoSum(new int[] { 3, 2, 4 }, 6);
+            Console.WriteLine($"Test 2: [{test2[0]}, {test2[1]}]");
+
+            var test3 = new Solution().TwoSum(new int[] { 3, 3 }, 6);
+            Console.WriteLine($"Test 3: [{test3[0]}, {test3[1]}]");
+        }
+
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    for (int j = 1; j < nums.Length; j++)
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            return new int[] { i, j };
+                        }
+
+                    }
+                }
+                throw new System.Exception("No solution found");
+            }
+        }
+    }
+}
